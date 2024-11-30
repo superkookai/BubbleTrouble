73dda7cbfd7cb09789d9d3f24ef83638fab8adfb
Weerawut <weerawut@live.com>
2024-11-30 12:56:07 +0700

    Initial Commit

---
+++ BubbleTrouble/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  BubbleTrouble
+//
+//  Created by Weerawut Chaiyasomboon on 30/11/2567 BE.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
